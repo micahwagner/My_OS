db36368542215f84ac3eb21c9809df7340e79a1c
Micah Wagner <micah.wagner@icloud.com>
2022-09-03 09:37:21 -0700

    added generic directory to init_docker.sh

--- build/init_docker.sh
+++ build/init_docker.sh
@@ -1,2 +1,3 @@
-docker run --rm -v /Users/micahwagner/Projects/LowLevelProjects/OS:/OS micahwagner/cross_compiler:latest bash -c 'cd OS/build/ && sh docker_script.sh'
-#docker run -it --rm -v /Users/micahwagner/Projects/asm/OS:/OS micahwagner/cross_compiler:latest /bin/bash 
+dir=$(cd ../ && pwd)
+docker run --rm -v $dir:/OS micahwagner/cross_compiler:latest bash -c 'cd OS/build/ && sh docker_script.sh'
+#docker run -it --rm -v $dir:/OS micahwagner/cross_compiler:latest /bin/bash 
