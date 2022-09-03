a0237f5c56fcb0948de9088a91d3de6ff25be84f
Micah Wagner <micah.wagner@icloud.com>
2022-09-02 17:33:59 -0700

    chnaged cross compiler to custom one

--- build/Execute_Kernel_Compiler.sh
+++ build/Execute_Kernel_Compiler.sh
@@ -1,2 +1,2 @@
-docker run --rm -v /Users/micahwagner/Projects/LowLevelProjects/OS:/OS kevincharm/i686-elf-gcc-toolchain:5.5.0 bash -c 'cd OS/build/ && make -f Compile_Kernel.mk'
+docker run --rm -v /Users/micahwagner/Projects/LowLevelProjects/OS:/OS micahwagner/cross_compiler:latest bash -c 'cd OS/build/ && make -f Compile_Kernel.mk'
 #docker run -it --rm -v /Users/micahwagner/Projects/asm/OS:/OS kevincharm/i686-elf-gcc-toolchain:5.5.0 /bin/bash
